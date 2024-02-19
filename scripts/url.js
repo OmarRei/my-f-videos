f853c38917768772342923fd00a8330105eaf5ef
OmarRei <155950527+OmarRei@users.noreply.github.com>
2024-02-19 08:26:15 +0200

    3.1.4

--- scripts/url.js
+++ scripts/url.js
@@ -1,34 +1,29 @@
-        // Function to generate a random query parameter
-        function generateRandomQuery() {
-            var query = '';
-            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            var charactersLength = characters.length;
-            for (var i = 0; i < 8; i++) {
-                query += characters.charAt(Math.floor(Math.random() * charactersLength));
-            }
-            return query;
-        }
+// Function to generate a random query parameter
+function generateRandomQuery() {
+    var query = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 8; i++) {
+        query += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return query;
+}
 
-        // Function to modify the URL pathname
-        function modifyURL() {
-            // Get the current URL
-            var url = new URL(window.location.href);
+// Function to generate a random path
+function generateRandomPath() {
+    var path = 'main.html'; // Maintain the base path
+    return path;
+}
 
-            // Get the pathname (part between the slashes)
-            var pathname = url.pathname;
+// Delay execution by 5 seconds
+setTimeout(function() {
+    // Generate a new random query parameter and path
+    var newQuery = generateRandomQuery();
+    var newPath = generateRandomPath();
 
-            // Modify the pathname as needed, in this case, just append "_modified"
-            var modifiedPathname = pathname.replace(/\(.+?\)/, '(modified)');
-
-            // Update the URL with the modified pathname
-            url.pathname = modifiedPathname;
-
-            // Update the URL with a random query parameter
-            url.searchParams.set('query', generateRandomQuery());
-
-            // Replace the current URL with the modified URL
-            window.history.replaceState({}, '', url.toString());
-        }
-
-        // Call the function to modify the URL
-        modifyURL();
+    // Update the URL with the new query parameter and path
+    var url = new URL(window.location.href);
+    url.searchParams.set('query', newQuery);
+    url.pathname = newPath;
+    window.history.replaceState({}, '', url.toString());
+}, 5000); // 5000 milliseconds = 5 seconds
